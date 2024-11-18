5f119bb4a9c43e7062d7b0ffa915deee6fe2050a
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 20:15:42 -0300

    feat: added correction activity05

--- atividades/atividade05.py
+++ atividades/atividade05.py
@@ -5,7 +5,7 @@ numero = float(input("Digite um número: "))
 outro_numero = float(input("Digite outro número: "))
 
 # Somando os dois números
-soma = (numero) + (outro_numero)
+soma = numero + outro_numero
 
 # Exibição
 print(f"A soma dos dois números é: {soma}")
