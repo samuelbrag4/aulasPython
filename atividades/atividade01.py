1c3b01ad2853e1568bbbf57e8da35727c103a929
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 13:48:09 -0300

    feat: added activity01

---
+++ atividades/atividade01.py
@@ -0,0 +1,55 @@
+# 1) Crie um programa que armazene o seu nome em uma variável e, em seguida, imprima uma
+
+# Nome
+nome = "Samuel dos Santos Braga"
+
+# print(f"Olá {nome}. Seja bem vindo!")
+
+# Mensagem
+print("Olá " + nome + ". Seja bem vindo!!")
+
+# 2) Crie um programa que armazene dois números em variáveis e imprima a soma desses números.
+
+# Variáveis que armazenam dois números
+a, b = 10, 20
+
+print(a + b)
+
+# 3) Crie um programa que armazene uma mensagem inicial e outra final em variáveis. Em seguida, imprima a mensagem concatenada.
+
+# Mensagem inicial
+mensagem_inicial = "Hello"
+
+# Mensagem final
+mensagem_final = "World!!"
+
+# Mensagem concatenada
+print(mensagem_inicial + " " + mensagem_final)
+
+# 4) Uma agência de viagens pretende realizar um voo para as Bahamas. O custo de operação do avião é de 5.000 dólares. Até o momento, 29 pessoas se inscreveram para a viagem. Se a agência cobra 200 dólares por passagem, qual será o lucro obtido pela empresa? Crie variáveis para representar cada quantidade numérica e utilize operações aritméticas apropriadas.
+
+# Variável de passageiros
+passageiros = 29
+
+# Valor da passagem
+valor_passagem = 200
+
+# Lucro total da empresa
+lucro_viagem = passageiros * valor_passagem
+
+# Exibição do lucro
+print(f"O lucro obtido pela empresa é de: {lucro_viagem}")
+
+# 5) Na Escola Senai, a turma de Informática está organizando um evento online. Eles decidiram vender camisetas personalizadas para arrecadar fundos. Cada camiseta custa 25 reais, e a turma planeja vender 50 camisetas. Crie um programa simples em Python que utilize variáveis para representar o preço da camiseta, a quantidade planejada de camisetas para venda e calcule o total arrecadado pela turma. Em seguida, imprima o resultado.
+
+# Camisetas
+custo_camiseta = 25
+
+# Plano de vendas
+plano_vendas = 50
+
+# Calculo de total arrecadado pela turma
+total_arrecadado = custo_camiseta * plano_vendas
+
+# Exibição do resultado
+print(f"O total arrecadado pela turma foi de: {total_arrecadado}")
