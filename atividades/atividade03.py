ca1ad434fce5f90ceee5b63872fa4e804df3b019
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 19:15:55 -0300

    feat: added activity03

---
+++ atividades/atividade03.py
@@ -0,0 +1,11 @@
+# Solicitação de inserção do primeiro nome
+primeiro_nome = input("Digite seu priemrio nome: ")
+
+# Solicitação de inserção do sobrenome
+sobrenome = input("Digite seu sobrenome: ")
+
+# Variável que conncatena o primeiro nome com o sobrenome
+nome_completo = primeiro_nome + " " + sobrenome
+
+# Exibição do resultado utilizando f-strings
+print(f"Seu nome completo é: {nome_completo}")
