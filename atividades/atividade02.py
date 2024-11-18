695749c4a9f3bfce73fe344a79968b6e5029d2b8
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 19:02:09 -0300

    feat: correction activity02

--- atividades/atividade02.py
+++ atividades/atividade02.py
@@ -3,3 +3,9 @@ frase = input("Digite a frase: ")
 ultima_letra = (frase[-1])
 
 print(ultima_letra)
+
+# Forma mais simples
+
+frase = input("Digite a frase: ")
+
+print("A última letra da frase é:", frase[-1])
