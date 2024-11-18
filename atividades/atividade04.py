2c6597c052d92235b594f3ca6c1e10f8f862eb84
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 19:43:55 -0300

    feat: added correction activity04

--- atividades/atividade04.py
+++ atividades/atividade04.py
@@ -1,10 +1,20 @@
 # Solicitação da inserção de uma frase
 frase = input("Digite uma linguagem de programação que você gosta: ")
 
+# Solicitação da inserção de uma nova linguagem
 nova_palavra = input("Digite outra linguagem: ")
 
+# Solicitação da inserção da linguagem que foi inserida anteriormente
 antiga_palavra = input("Digite a linguagem que você inseriu anteriormente: ")
 
+# Nova frase, que junta a frase inserida com a nova palavra
 nova_frase = frase.replace(antiga_palavra, nova_palavra)
 
+# Exibição 
 print(f"A nova frase é: {nova_frase.capitalize()}")
+
+'''
+Uma forma mais fácil é só adicionar o valor da variável "nova_frase" dentro do print:
+print(f"A nova frase é: {frase.replace(antiga_palavra, nova_palavra)}")
+
+'''
