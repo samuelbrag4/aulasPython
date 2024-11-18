7975a8e56dd2912d3884b71bba050c1f42269b7f
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-18 20:05:23 -0300

    feat: added activity05

---
+++ atividades/atividade05.py
@@ -0,0 +1,11 @@
+# Solicitção da inserção de um número (o float é para aceitar números decimais)
+numero = float(input("Digite um número: "))
+
+# Solicitção da inserção de outro número (o float é para aceitar números decimais)
+outro_numero = float(input("Digite outro número: "))
+
+# Somando os dois números
+soma = (numero) + (outro_numero)
+
+# Exibição
+print(f"A soma dos dois números é: {soma}")
