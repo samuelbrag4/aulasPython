958eacb1677c950e6dc6d727f1065d7f835fc2e3
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-19 19:29:41 -0300

    feat: Adding data of different types to a list

--- aulas/aula03/lista.py
+++ aulas/aula03/lista.py
@@ -17,3 +17,21 @@ print(type(frutas))
 print(frutas[0])
 print("----------")
 print(frutas[1])
+
+# ----------
+
+# Declarar outros tipos de dados em uma lista
+
+'''
+Na lista podem ser armazenados diferentes tipos de dados, como inteiros, strings, booleanos, floats, etc.
+Pode-se declarar em uma lista diferentes tipos de dados, que tem funções completamente diferentes. Como por exemplo, armazenar um texto e a soma de dois números.
+Segue exemplos:
+'''
+
+a_lista = ["Banana", 1, 2.5, True, "Nome", 4==4, 1 + 2]
+
+# Exibição da lista
+print(a_lista)
+
+# Exibiçãom do tipo da variável - "list"
+print(type(a_lista))
