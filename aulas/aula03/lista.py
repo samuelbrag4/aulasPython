c10118c065b9d66e2c27458a7e487f238be15c4b
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-19 19:20:15 -0300

    feat: added aula03 lists in Python

---
+++ aulas/aula03/lista.py
@@ -0,0 +1,14 @@
+'''
+Uma lista é uma coleção de itens em uma ordem particular.
+A lista deve ser declarada/criada dentro de colchetes([]), e os elementos dela devem ser separados por vírgula.
+Segue um exemplo de declaração de uma lista de frutas:
+'''
+
+# Declaração de uma lista
+frutas = ["Banana", "Maçã", "Pera", "Uva", "Morango"]
+
+# Exibição da lista
+print(frutas)
+
+#Exibiçãom do tipo da variável - "list"
+print(type(frutas))
