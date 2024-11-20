ed3e2fce978be4f1859d8eca18238876f2948586
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-19 21:58:06 -0300

    feat: activity06 - cities in the north and south

---
+++ atividades/atividade06.py
@@ -0,0 +1,21 @@
+# Duas listas de cidades do Norte e Sul do Brasil
+
+# Cidades do Norte
+norte = ["Manaus", "Belém", "Palmas", "Porto Velho", "Boa Vista", "Macapá", "Rio Branco"]
+
+# Cidades do Sul
+sul = ["Curitiba", "Florianópolis", "Porto Alegre", "Londrina", "Joinville", "Pelotas", "Criciúma"]
+
+# Lista geral
+cidades = [norte, sul]
+
+# Exibições das listas
+
+# Norte
+print(f"Cidades do Norte: {norte}")
+
+# Sul
+print(f"Cidades do Sul: {sul}")
+
+# Geral
+print(f"Cidades Gerais: {cidades}")
