e6a7ac77efd34f76da1a320b667f5e80f41f8ae3
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-19 22:10:58 -0300

    feat: activity06 with .extend() method

--- atividades/atividade06.py
+++ atividades/atividade06.py
@@ -19,3 +19,39 @@ print(f"Cidades do Sul: {sul}")
 
 # Geral
 print(f"Cidades Gerais: {cidades}")
+
+print("\n")
+print("-----SEPARAÇÃO-----")
+print("\n")
+
+# ----------COM EXTEND()
+
+# Cidades do Norte
+norte = ["Manaus", "Belém", "Palmas", "Porto Velho", "Boa Vista", "Macapá", "Rio Branco"]
+
+# Cidades do Sul
+sul = ["Curitiba", "Florianópolis", "Porto Alegre", "Londrina", "Joinville", "Pelotas", "Criciúma"]
+
+# Lista geral
+cidades.extend(norte)
+cidades.extend(sul)
+
+# Exibições das listas
+print(f"Cidades do Norte: {norte}")
+print(f"Cidades do Sul: {sul}")
+print(f"Cidades Gerais: {cidades}")
+
+# -----OU-----
+
+print("\n")
+print("-----SEPARAÇÃO-----")
+print("\n")
+
+# Lista geral
+cidades.extend(["Manaus", "Belém", "Palmas", "Porto Velho", "Boa Vista", "Macapá", "Rio Branco"])
+cidades.extend(["Curitiba", "Florianópolis", "Porto Alegre", "Londrina", "Joinville", "Pelotas", "Criciúma"])
+
+# Exibições das listas
+print(f"Cidades do Norte: {norte}")
+print(f"Cidades do Sul: {sul}")
+print(f"Cidades Gerais: {cidades}")
