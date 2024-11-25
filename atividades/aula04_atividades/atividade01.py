36038be8e3c077b4d7078ab5273f34f96f1679ff
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-25 19:17:04 -0300

    feat: update atividade01.py to include explanation of sorted() function

--- atividades/aula04_atividades/atividade01.py
+++ atividades/aula04_atividades/atividade01.py
@@ -18,4 +18,8 @@ EXPLICAÇÃO:
 O "set()" serve para criar um conjunto em Python, que é uma coleção não ordenada de elementos únicos. 
 Ao converter uma lista para um set, todas as duplicatas são removidas automaticamente. 
 Isso é útil quando queremos obter apenas os valores exclusivos de uma lista.
+
+O "sorted()" serve para deixar os valores de uma lista em ordem crescente (ou de acorodo com algum parâmetro que for passado).
+Ao converter a lista com o sorted, os valores são organizados em ordem crescente.
+Muito útil para manter sempre a organização 
 '''
