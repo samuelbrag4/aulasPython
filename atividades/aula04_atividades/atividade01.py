03e1f8deabd674181b19efcc9f98ed86bd7f50ee
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-25 19:07:44 -0300

    feat: add atividade01.py to obtain unique values from a list using set

---
+++ atividades/aula04_atividades/atividade01.py
@@ -0,0 +1,21 @@
+# Escreva um programa em Python para obter valores exclusivos de uma lista. (set)
+
+# Exemplo de lista com valores duplicados
+lista = [10, 20, 30, 40, 20, 50, 60, 40]
+
+# Convertendo a lista para um set para obter valores exclusivos
+valores_unicos = set(lista)
+
+# Convertendo de volta para lista, se necessário
+valores_unicos_lista = sorted(list(valores_unicos))
+
+# Exibindo os valores exclusivos
+print(f"Os valores únicos da lista são: \n{valores_unicos_lista} \nEles estão em ordem crescente!")
+
+'''
+EXPLICAÇÃO:
+
+O "set()" serve para criar um conjunto em Python, que é uma coleção não ordenada de elementos únicos. 
+Ao converter uma lista para um set, todas as duplicatas são removidas automaticamente. 
+Isso é útil quando queremos obter apenas os valores exclusivos de uma lista.
+'''
