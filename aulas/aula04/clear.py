ef61d57f04274fdd52e300503e79a05b6ecbca47
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:30:12 -0300

    feat: add clear() method to clear dictionary in clear.py

---
+++ aulas/aula04/clear.py
@@ -0,0 +1,16 @@
+# Método clear() para limpar o dicionário
+
+# Criação do dicionário
+dict1 = {
+    "Pessoa01": ["william", 30, "Professor"],
+    "Pessoa02": ["Julia", 50, "Médica"]
+}
+
+# Exibição
+print(dict1)
+
+# Utilizando o método clear()
+dict1.clear()
+
+# Nova exibição
+print(dict1)
