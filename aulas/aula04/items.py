965d7d3cf7a7ea0d5241bfa5c40dcde1432fd539
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:28:34 -0300

    feat: add items method demonstration in items.py

---
+++ aulas/aula04/items.py
@@ -0,0 +1,13 @@
+# items() faz a leitura de todos os itens do dicionário.
+
+# Criação do dicionário com valores
+dict1 = {
+    "Pessoa01": ["william", 30, "Professor"],
+    "Pessoa02": ["Julia", 50, "Médica"]
+}
+
+# Exibição
+print(dict1)
+
+# Utilizando o método items()
+print(dict1.items())
