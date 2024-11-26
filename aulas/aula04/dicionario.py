c4a5a425103ba483e5331b46a1405eea94b845f4
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-25 21:29:31 -0300

    feat: add dicionario.py with a sample dictionary and type demonstration

---
+++ aulas/aula04/dicionario.py
@@ -0,0 +1,14 @@
+# Dicionário 
+
+# Criação do dicionário
+meus_dados = {
+    "nome": "Samuel",
+    "Idade": 16,
+    "Programa": True
+}
+
+# Exibição do dicionário
+print(meus_dados)
+
+# Exibição do tipo
+print(type(meus_dados))
