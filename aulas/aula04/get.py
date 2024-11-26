ce10a3dbf910044d18cc28be26c893fee98a3daf
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:04:59 -0300

    feat: add get method in get.py to access values ​​from a dictionary

---
+++ aulas/aula04/get.py
@@ -0,0 +1,14 @@
+# Método get para acessar um valor de uma chave
+
+# Criação do dicionário
+meus_dados = {
+    "nome": "Samuel",
+    "Idade": 16,
+    "Programa": True
+}
+
+# Exibição do dicionário
+print(meus_dados)
+
+# get 
+print(meus_dados.get("nome"))
