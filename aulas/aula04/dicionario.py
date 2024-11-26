e15b7276e50e2677733f6607f4db43b3241085ca
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-25 21:56:27 -0300

    feat: enhance dicionario.py with a demonstration of adding values to an empty dictionary

--- aulas/aula04/dicionario.py
+++ aulas/aula04/dicionario.py
@@ -21,3 +21,17 @@ print(persona)
 print(type(persona))
 
 print(f"O nome é: {persona["nome"]}, do sexo: {persona["sexo"]}")
+
+# Dicionário vasio e adicionando valores nele
+
+# Declaração do dicionário
+dict1 = {}
+
+# Exibição dele
+print(dict1)
+
+# Adicionando um dado ao dicionário vazio
+dict1["chave1"] = "Não está mais vazio!!"
+
+# Novo dicionário 
+print(dict1)
