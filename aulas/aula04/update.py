645d8f5f11131f2cb7824fdedda226516d93dbb2
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:52:40 -0300

    feat: add example of updating dictionary with another dictionary in update.py

--- aulas/aula04/update.py
+++ aulas/aula04/update.py
@@ -21,3 +21,27 @@ dict1.update({"d": 7})
 
 # Nova exibição
 print(dict1)
+
+# Outra forma de fazer
+
+# Dicionário original 
+dicionario1 = {
+    "a": 10,
+    "b": 1,
+    "c": 2
+}
+
+# Exibir
+print(dicionario1)
+
+# Dicionário que será adicionado
+dicionario2 = {
+    "b": 5,
+    "d": 7
+}
+
+# Adicona elementos de outro dicionário
+dicionario1.update(dicionario2)
+
+# Exibir
+print(dicionario1)
