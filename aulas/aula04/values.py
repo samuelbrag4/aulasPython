2b3ac06ee4fa0f2c605fbb21ec36a2bd05dcbf63
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:26:29 -0300

    feat: add example of using the values() method in values.py

---
+++ aulas/aula04/values.py
@@ -0,0 +1,14 @@
+# values() é utilizado para retornar os valores de um dicionário
+
+# Criação do dicionário
+meus_dados = {
+    "nome": "Samuel",
+    "Idade": 16,
+    "Programa": True
+}
+
+# Exibição
+print(meus_dados)
+
+# Utilização do método values()
+print(meus_dados.values())
