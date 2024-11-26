55ae28734e61ab94e7e6b4f598513b46bacd418a
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-26 19:39:27 -0300

    feat: add del() method demonstration to remove an item from a dictionary in del.py

---
+++ aulas/aula04/del.py
@@ -0,0 +1,17 @@
+# Método del() para remover um dicionário
+
+# Criação do dicionário
+dict1 = {
+    "a": 10,
+    "b": 1,
+    "c": 2
+}
+
+# Exibição
+print(dict1)
+
+# Utilizando o método del()
+del dict1["b"]
+
+# Nova exibição
+print(dict1)
