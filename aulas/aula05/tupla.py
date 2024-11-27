d10b2ae04c1aaa6f0ab7e5308cae4278e5227db0
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 19:14:56 -0300

    feat: add detailed explanations about tuples and usage examples

---
+++ aulas/aula05/tupla.py
@@ -0,0 +1,38 @@
+# TUPLA - é uma coleção de elementos ordenada e imutável (semelhante a lista).
+
+'''
+ESTRUTURA:
+
+nomeTupla = (elemento1, elemento2, elemento3, ..., elementoN)
+
+EXEMPLO:
+tupla = (1, 2, 3, 4, ..., n)
+
+CARACTERÍSTICAS:
+
+- Tuplas são imutáveis, ou seja, não podem ser alteradas após a sua criação.
+- Tuplas são ordenadas, ou seja, os elementos possuem uma ordem definida.
+- Tuplas permitem elementos duplicados.
+- Tuplas são definidas por parênteses ().
+'''
+
+# CRIANDO UMA TUPLA
+tupla = (1, 2, 3, 4, 5)
+
+# ACESSANDO ELEMENTOS DE UMA TUPLA
+print(tupla[0]) # Saída: 1
+
+'''
+EXPLICAÇÃO DE ACESSO:
+
+- O acesso aos elementos de uma tupla é feito através de um índice.
+- O índice de uma tupla começa em 0.
+
+EXEMPLO:
+
+tupla = (1, 2, 3, 4, 5)
+
+- tupla[0] -> 1
+
+nomeTupla[índice] -> elemento
+'''
