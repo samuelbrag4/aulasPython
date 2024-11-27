f66ea41edf961ee86e3001178c200628de3df910
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 20:10:04 -0300

    feat: add example of creating tuple with different data types

---
+++ aulas/aula05/pratica.py
@@ -0,0 +1,7 @@
+# Escreva um programa em python para criar uma tupla com diferentes tipos de dados.
+
+# Criando uma tupla com diferentes tipos de dados
+tupla = (1, 'Python', 3.14, True, 'Java', 2.71, False, 'C++', 0.0, 'C#', 0, 'Next')
+
+# Exibindo a tupla
+print(tupla)
