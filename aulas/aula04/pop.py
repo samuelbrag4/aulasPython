3c292596b90c756785c275d78531ab9d4eb6a1f2
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2024-11-27 18:58:34 -0300

    feat: add example of pop() method for list manipulation

---
+++ aulas/aula04/pop.py
@@ -0,0 +1,19 @@
+# Método: POP() - Remove o último elemento da lista
+
+# Definindo a lista
+lista = [1, 2, 3, 4, 5]
+
+# Exibindo a lista
+print(lista)
+
+# Removendo o último elemento da lista
+lista.pop()
+
+# Nova lista
+print(lista)
+
+# Pop() com índice
+lista.pop(1)
+
+# Nova lista
+print(lista)
