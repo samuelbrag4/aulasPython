d5e8a6f70d472f098085a5eb316a9d8bce2db0a1
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-04 19:46:56 -0300

    feat: add examples of while loops for counting and summing odd numbers

--- aulas/aula07/while.py
+++ aulas/aula07/while.py
@@ -25,3 +25,32 @@ Explicação do fluxo do código:
 4. O loop termina quando o "i" for maior que o "n"
 5. O resultado é impresso na tela
 '''
+
+# Exemplo02 --> Loop de while que imprima números de 1 a 5 em ordem crescente
+numero_usuario = int(input("Digite um número: "))
+
+numero_sistema = 1
+
+print('Iniciando a contagem...')
+
+while numero_sistema <= numero_usuario:
+    print(numero_sistema)
+    numero_sistema += 1
+    
+print('Fim da contagem.')
+
+# Exemplo03 --> Loop que calcula e imprime a soma dos números ímpares de 1 até um número inserido pelo usuário
+numero_inserido = int(input("Digite um número: "))
+
+soma = 0
+
+contador = 1
+
+while contador <= numero_inserido:
+    if contador % 2 != 0:
+        soma += contador
+        contador += 1
+    else:
+        contador += 1
+        
+print(f'A soma dos números ímpares de 1 até {numero_inserido} é {soma}')
