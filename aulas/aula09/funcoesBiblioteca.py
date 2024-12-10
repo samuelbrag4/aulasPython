5eb82bcad3d2138ae8c56c219790faa5e38f95bc
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-09 21:16:54 -0300

    feat: add library functions module with usage examples

---
+++ aulas/aula09/funcoesBiblioteca.py
@@ -0,0 +1,25 @@
+# Módulos e Funções de Biblioteca
+
+'''
+Uma biblioteca é um conjunto de módulos que podem ser importados para o seu programa.
+
+Algumas bibliotecas populares e o que fazem:
+1- math: funções matemáticas
+2- random: funções para gerar números aleatórios
+3- time: funções para manipular datas e horas
+4- os: funções para manipular arquivos e diretórios
+5- sys: funções para manipular o interpretador Python
+
+Para importar uma biblioteca, use o comando import seguido do nome da biblioteca.
+
+Exemplo:
+import math # importa a biblioteca math
+'''
+
+from math import sqrt
+
+# -----sqrt----- é uma função da biblioteca math que calcula a raiz quadrada de um número.
+
+resultado = sqrt(25)
+print(resultado)
+
