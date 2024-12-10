ebaeecff3e554aaa40688b0f35ab5f0ac3564b3d
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-09 21:02:53 -0300

    feat: add example of using lambda functions to calculate the square of numbers

---
+++ aulas/aula09/mePerdi.py
@@ -0,0 +1,23 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+ao_quadrado = list(map(lambda x: x ** 2, numeros))
+
+print(ao_quadrado)
+
+'''
+Lambda:
+
+- Função anônima
+- Não precisa de nome
+- Pode ter vários argumentos
+- Pode ter várias linhas de código
+- Pode ser atribuída a uma variável
+
+Sintaxe:
+lambda argumentos: expressão
+'''
+
+# Exemplo: Lambda
+quadrado = lambda x: x ** 2
+
+print(quadrado(2))
