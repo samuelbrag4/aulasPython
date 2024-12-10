cd3d3890e1524221c0bb1700ffcb2c804a96d36e
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-09 21:43:57 -0300

    feat: add example of using the try-except structure in Python

---
+++ aulas/aula09/tryExept.py
@@ -0,0 +1,12 @@
+# TryExcept
+
+'''
+Estrutura:
+
+try:
+    # Código que pode gerar uma exceção
+    # ...
+except tipo_execucao as nome_da_excecao:
+    # Código que será executado caso ocorra uma exceção
+    # ...
+'''
