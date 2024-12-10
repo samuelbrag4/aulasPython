edfca6b816fd90540de76e854cdc7b8e1d470032
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-09 22:00:46 -0300

    feat: add example of writing files in Python

--- atividades/manipulacaoArquivos/main.py
+++ atividades/manipulacaoArquivos/main.py
@@ -4,3 +4,10 @@ with open('atividades/manipulacaoArquivos/samuelPython.txt', 'r') as arquivo:
     print(conteudo)
     
 # O bloco with garante que o arquivo seja fechado corretamente após a execução do bloco de código.
+
+# Gravação de arquivos
+
+with open('novo_arquivo.txt', 'w') as arquivo:
+    arquivo.write('Este é um novo arquivo criado com Python!\n')
+    arquivo.write('Python é uma linguagem de programação muito poderosa!\n')
+    arquivo.write('Estou aprendendo a programar em Python!\n')
