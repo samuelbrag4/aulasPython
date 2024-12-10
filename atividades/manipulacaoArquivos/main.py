e800898f3202bdbfe12559f4034a575eeb0a0852
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-09 22:07:09 -0300

    feat: add functionality to rename and read files in Python

--- atividades/manipulacaoArquivos/main.py
+++ atividades/manipulacaoArquivos/main.py
@@ -1,13 +1,22 @@
-# Usando o bloco with para garantir que o arquivo seja fechado corretamente
+# Leitura de arquivos
 with open('atividades/manipulacaoArquivos/samuelPython.txt', 'r') as arquivo:
     conteudo = arquivo.read()
     print(conteudo)
-    
+
 # O bloco with garante que o arquivo seja fechado corretamente após a execução do bloco de código.
 
 # Gravação de arquivos
-
 with open('novo_arquivo.txt', 'w') as arquivo:
     arquivo.write('Este é um novo arquivo criado com Python!\n')
     arquivo.write('Python é uma linguagem de programação muito poderosa!\n')
     arquivo.write('Estou aprendendo a programar em Python!\n')
+
+# Renomear arquivo
+import os
+os.rename('novo_arquivo.txt', 'novo_arquivo_renomeado.txt')
+print('Novo arquivo renomeado com sucesso!')
+
+# Leitura do arquivo renomeado
+with open('novo_arquivo_renomeado.txt', 'r') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
