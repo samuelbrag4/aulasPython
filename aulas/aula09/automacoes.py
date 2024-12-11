3657fda50975f6933b75f2460d5982eed9ed27e2
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-11 19:14:22 -0300

    feat: add automations.py file with advantages of using Python for automations

---
+++ aulas/aula09/automacoes.py
@@ -0,0 +1,15 @@
+# Automações em Python
+
+'''
+Vantagens de usar python para automações:
+
+- Facilidade de aprendizado
+- Linguagem de auto nível (ou seja, fácil de entender)
+- Comunidade ativa
+- Bibliotecas prontas para automações
+- Multiplataforma
+- Gratuito
+- Open Source
+- Fácil de instalar
+- Fácil de usar
+'''
