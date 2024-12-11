9f2aad3aec820e8b8600a4d58c4347aaacafcaad
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-11 19:35:53 -0300

    feat: add Excel file handling in automations.py

--- aulas/aula09/automacoes.py
+++ aulas/aula09/automacoes.py
@@ -14,7 +14,31 @@ Vantagens de usar python para automações:
 - Fácil de usar
 '''
 
-# Para realizar automações, vamos utilizar duas bibliotecas. Sendo elas:
-
-import Openpyxl # pip install openpyxl
 # Essa biblioteca é utilizada para manipular arquivos do Excel
+from openpyxl import load_workbook
+
+# Caminho para o arquivo do excel
+arquivo = 'C:/Users/Aluno/Desktop/cursoPython/exelCursoPython.xlsx'
+
+# Carregar o arquivo do excel
+wb = load_workbook(arquivo)
+
+# Selecionar a planilha ativa (a primeira planilha por padrão)
+planilha = wb.active 
+
+# Iterar sobre as células da planilha e imprimir seus valores
+for linha in planilha.iter_rows(values_only=True):
+    print(linha)
+    
+'''
+Explicação da lógica e dos simbolos utilizados no código abaixo:
+
+for linha in planilha.iter_rows(values_only=True):
+    print(linha)
+    
+Enquanto houver linhas na planilha, faça:
+    Imprima a linha
+
+O .iter_rows() é um método que itera sobre as linhas da planilha
+O values_only=True é um argumento que retorna apenas os valores das células
+'''
