6662a96745c1823e93c836ae16c078fce02c07d9
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-11 21:04:55 -0300

    feat: add joiningSheets.py script to combine multiple Excel sheets into a single one

---
+++ aulas/aula09/juntandoPlanilhas.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import glob
+import os
+
+# Caminho para onde os arquivos do exel estão salvos
+caminho = 'C:/Users/Aluno/Desktop/cursoPython/semana/'
+arquivos = glob.glob(os.path.join(caminho, "*.xlsx"))
+
+# Lista para armazenar os DataFrames individuais
+consolidado = []
+
+# Loop pelos arquivos para combinar
+for arquivo in arquivos:
+    # Carregar o arquivo do Exel
+    df = pd.read_excel(arquivo)
+    
+    # Extrair o nome do arquivo para identificar a semana
+    nome_arquivo = os.path.basename(arquivo).split('.')[0]
+    df['Semana'] = nome_arquivo # Adicona uma coluna com a identificação da semana
+    
+    # Adicionar o DataFrame a lista
+    consolidado.append(df)
+    
+# Combinar todos os DataFrames em um único
+df_completo = pd.concat(consolidado, ignore_index=True)
+
+# Salvar o resultado consolidado em um único Exel
+df_completo.to_excel('C:/Users/Aluno/Desktop/cursoPython/semana/consolidado.xlsx', index=False)
+
+print('Planilhas combinadas com sucesso!')
